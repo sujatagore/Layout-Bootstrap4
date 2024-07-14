137606e174b465aa2bb650b1b9994b78c79c5998
Sujata Gore <goress342@gmail.com>
2024-07-15 01:07:06 +0530

    aboutUs commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+const cl = console.log;
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    navText: ['<i class="fa-solid fa-square-caret-left fa-2x"></i>', '<i class="fa-solid fa-square-caret-right fa-2x"></i>'],
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            dots:false
+        },
+        600:{
+            items:3,
+            nav:false,
+            dots:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            dots:true
+        }
+    }
+})
